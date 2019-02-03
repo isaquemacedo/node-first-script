ca7aa41b3f32aba6285181725b4b9a8aca7edd3f
Isaque Alves <isaque.mace@gmail.com>
2019-02-03 21:27:42 -0200

    Exemplo de objeto global e process

---
+++ first-script.js
@@ -0,0 +1,17 @@
+console.log('n-fatorial')
+
+/* console.log(`Executando o script a partir do diretorio ${process.cwd()}`)
+
+process.on('exit', () => {
+    console.log('script está prestes a terminar')
+}) */
+
+const fatorial = (num) => {
+    if(num === 0) return 1
+
+    return num * fatorial(num - 1)
+}
+
+const num =  parseInt(process.argv[2])
+
+console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
